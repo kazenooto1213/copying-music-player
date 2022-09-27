c87f0bb3d8a356d6051b5cdce6deeabe5cad9c25
kazenooto1213 <111048618+kazenooto1213@users.noreply.github.com>
2022-09-27 16:09:24 +0900

    music

---
+++ js/music-list.js
@@ -0,0 +1,48 @@
+let allMusic = [
+{
+  name: "優しい灯火(Tenderly_glow)",
+  artist: "蒲鉾さちこ",
+  img: "優しい灯火(Tenderly_glow)",
+  src: "優しい灯火(Tenderly_glow)",
+},
+{
+  name: "魂の還る場所",
+  artist: "蒲鉾さちこ",
+  img: "魂の還る場所",
+  src: "魂の還る場所",
+},
+{
+  name: "秋晴れの空に",
+  artist: "蒲鉾さちこ",
+  img: "秋晴れの空に",
+  src: "秋晴れの空に",
+},
+{
+  name: "光差し込む森",
+  artist: "蒲鉾さちこ",
+  img: "光差し込む森",
+  src: "光差し込む森",
+},
+{
+  name: "おとぼけダンス(Silly_dance)",
+  artist: "蒲鉾さちこ",
+  img: "おとぼけダンス(Silly_dance)",
+  src: "おとぼけダンス(Silly_dance)",
+},
+{
+  name: "踊る、宇宙の中で(Dancing,at_Universe)",
+  artist: "蒲鉾さちこ",
+  img: "踊る、宇宙の中で(Dancing,at_Universe)",
+  src: "踊る、宇宙の中で(Dancing,at_Universe)"
+},
+
+// To add more song ,just copy the following code and paste inside the array
+// like this paste it , and remember to give comma after ending of this bracket }
+// {
+//   name: "曲名",
+//   artist: "アーティスト名",
+//   img: "曲の画像 (img,jpg)",
+//   src: "曲の音源"
+// },
+
+];
